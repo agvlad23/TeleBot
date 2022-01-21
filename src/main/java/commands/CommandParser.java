4b5fd2785958cc9f9125b346a5296f39e33d6c2f
vla <ag.vlad23@gmail.com>
2022-01-21 11:45:47 +0300

    prefix fix

--- src/main/java/commands/CommandParser.java
+++ src/main/java/commands/CommandParser.java
@@ -1,14 +1,55 @@
 package commands;
 import bot.TrackerBot;
-public class CommandParser {
 
-    public static Command getCommandClass(String s){
-        switch (s.split("\\s")[0]) {
-            case TrackerBot.botPrefix+"start":
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class CommandParser {
+   /* String chatId;
+
+
+
+    Map<String,String> aliases;
+
+    public CommandParser() {
+
+    }
+
+    public CommandParser(String chatId) {
+        this.chatId = chatId;
+
+    }
+
+    public void loadAlias(String chatId){
+
+    }
+
+    public void saveAlias(String chatId){
+
+    }
+
+    public void addAlias(String command,String alias){
+        aliases.put(alias,command);
+    }
+
+    public Command getCommandClassAliases(String alias){
+        if (aliases==null)
+            loadAlias(chatId);
+        String commandName=aliases.getOrDefault(alias,alias);
+        return getCommandClass(commandName);
+    }*/
+
+
+    public static Command getCommandClass(String commandName){
+        var k=StartCommand.class;
+
+        switch (commandName.split("\\s")[0].substring(1).toLowerCase()) {
+            case "start":
                 return new StartCommand();
-            case TrackerBot.botPrefix+"stop":
+            case "stop":
                 return new StopCommand();
-            case TrackerBot.botPrefix+"help":
+            case "help":
                 return new HelpCommand();
             default: return new HelpCommand();
         }
